1a9c23c498024628c32e2a5016ff2f8defc01ab2
Brondani-7 <victorbrondanids@gmail.com>
2025-11-07 10:53:45 -0300

    Implement Produto class to calculate final price

---
+++ calculaPreco.java
@@ -0,0 +1,20 @@
+public class Produto {
+    String nome;
+    double preco;
+    double desconto;
+    public Produto(String nome, double preco, double desconto) {
+        this.nome = nome;
+        this.preco = preco;
+        this.desconto = desconto;
+    }
+
+    public double calcularPrecoFinal() {
+        return preco - (preco * desconto / 100);
+    }
+
+    public static void main(String[] args) {
+        Produto produto = new Produto("Fone de ouvido", 200, 15);
+        System.out.printf("Preco inicial de %s: R$ %.2f%n", produto.nome, produto.preco);
+        System.out.printf("Preco final de %s: R$ %.2f%n", produto.nome, produto.calcularPrecoFinal());
+    }
+}
